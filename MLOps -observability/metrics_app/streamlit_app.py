6ff93f146179fbaee59fe5442e31ea73769db81f
Giorgia Bertacchini <giorgia.bertacchini92@gmail.com>
2022-10-31 13:47:37 +0100

    comunication with develop sector

--- MLOps -observability/metrics_app/streamlit_app.py
+++ MLOps -observability/metrics_app/streamlit_app.py
@@ -17,13 +17,42 @@ GRAFANA_URL = "http://localhost:3000/"
 f = open(os.path.join("datasets", "params.json"))
 params = json.load(f)
 
-st.title("Prediction request")
+def new_tag():
+    os.system('docker images --filter=reference=activities_model --format "table TAG={{.Tag}}" > tag.txt')
+
+    fp = open("tag.txt")
+    for i, line in enumerate(fp):
+        if i == 1:
+            ff=open(".env", "w")
+            ff.write(line)
+            ff.close()
+            break
+    fp.close()
+
+def new_reference():
+    response = requests.get(
+        "http://127.0.0.1:3030/dvc_file"
+    )
+    myFile = open("datasets/DATA.csv.dvc", "w")
+    myFile.write(response.text)
+    myFile.close()
+
+    logging.info("Pull reference dataset from Google Drive.")
+    os.system("dvc pull --force")        
+    logging.info("Success.")
+
+    st.caption("Pull reference dataset from Google Drive successfully.")
+    reference = pd.read_csv('datasets/DATA.csv')
+    st.write(reference)
+
+st.title("Production platform")
 
 st.header("Welcome, the model is ready!")
 
 flag = True
 
-tab1, tab2 = st.tabs(["Predictions", "Monitor"])
+tab1, tab2, tab3, tab4 = st.tabs(["Predictions", "Monitor", "Complete Retrain", "Single actions"])
+
 
 with tab1:    
     st.subheader("Upload file csv")
@@ -77,9 +106,9 @@ with tab1:
             prediction = prediction.replace("]", "")
             li = prediction.split(", ")
 
-            tab3, tab4 = st.tabs(["Predictions", "Output file csv"])
+            tab10, tab20 = st.tabs(["Predictions", "Output file csv"])
         
-            with tab3:
+            with tab10:
                 st.subheader("All predictions")
                 
                 count = 0
@@ -95,7 +124,7 @@ with tab1:
                     count += 1
                     st.markdown("""---""")
         
-            with tab4:
+            with tab20:
                 st.subheader("Create file csv with Upload file and prediction data")
 
                 df['Quality'] = li
@@ -120,3 +149,85 @@ with tab2:
     st.subheader("To see the Bentoml service:")
     
     st.write(f"Bentoml - {BENTOML_URL}")
+
+    st.subheader("To see alerts received:")
+    
+    st.markdown(f"Slack Chat in **_monitoring_** channel")
+
+with tab3:
+    st.subheader("Is drift verified?")
+    st.markdown("If yes, you should retrain the model with new dataset. Press the button.")
+
+    st.caption("What do the next:")
+    st.caption("    - Send new Dataset on train model,")
+    st.caption("    - Retrain the model (push dvc dataset, mlflow run, build and dockerize bento),")
+    st.caption("    - Update reference dataset in production,")
+    st.caption("    - Update model in production (update tag).")
+
+    new_dataset = st.file_uploader("Choose a file CSV as new reference dataset", type="csv")
+    if new_dataset is not None:
+        df_ref = pd.read_csv(new_dataset, sep = ",", header = 0,index_col = False)
+        st.write(df_ref)
+
+        df_ref_json = df_ref.to_json (orient = "records", default_handler = None)
+
+        if st.button("Load and retrain"):
+            logging.info("Load new dataset to create new model and retrain the model.")    
+            prediction = requests.post(
+                "http://127.0.0.1:3030/load_new_data",
+                headers={"content-type": "application/json"},
+                data=df_ref_json,
+            ).text  
+
+            logging.info("Update reference dataset.")
+            new_reference()
+
+            logging.info("Update tag about model.")      
+            new_tag()
+
+            logging.info("End. Complete retrain")
+
+
+with tab4:
+    st.subheader("Retrain model?")
+    st.markdown("If you should retrain the model with the same dataset.")
+    st.caption("Press the button to retrain the model.")
+    if st.button("Retrain only"):
+        logging.info("Retrain the model.")
+        response = requests.get(
+            "http://127.0.0.1:3030/retrain"
+        )
+        st.write("status code: ", response.status_code)        
+        logging.info("End Retrain")
+
+    st.markdown("""---""")
+
+    st.subheader("Build Bento?")
+    st.markdown("If you need new bento, yoou should build and dockerize bento")
+    st.caption("Press the button to build and dockerize bento.")
+    if st.button("Bento"):
+        logging.info("Build and dockerize bento.")
+        response = requests.get(
+            "http://127.0.0.1:3030/bento"
+        )
+        st.write("status code: ", response.status_code)        
+        logging.info("End Build Bento")
+    
+    st.markdown("""---""")
+
+    st.subheader("Reference dataset changed?")
+    st.markdown("If yes, you should update reference dataset of monitoring sector.")
+    st.caption("Press the button to pull dvc dataset from Google Drive.")
+    if st.button("Update Reference"):
+        new_reference()
+        logging.info("End Update Reference")
+
+    st.markdown("""---""")
+
+    st.subheader("Update model in monitoring sector?")
+    st.markdown("If you should update the model you need new model tag.")
+    st.caption("Press the button to update new model tag.")
+    if st.button("Update Model"):
+        logging.info("Search new model tag.")
+        new_tag()
+        logging.info("End Update Model.")
